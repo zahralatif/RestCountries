a7ffb6725559b80b500f3269339675d30edb8414
zahralatif <zahralatif2107@gmail.com>
2023-11-11 21:03:07 +0400

    localstorage

--- assets/js/theme-changer.js
+++ assets/js/theme-changer.js
@@ -1,5 +1,20 @@
 let themeChanger = document.querySelector(".header-theme-changer");
 
+// themeChanger.addEventListener("click", function () {
+//   themeChanger.offsetParent.classList.add("dark-theme");
+// });
+
+let body = document.body;
+
+const isDarkMode = localStorage.getItem("darkMode") === "true";
+
+if (isDarkMode) {
+  body.classList.add("dark-theme");
+}
+
 themeChanger.addEventListener("click", function () {
-  themeChanger.offsetParent.classList.add("dark-theme");
+  body.classList.toggle("dark-theme");
+
+  const isDarkModeEnabled = body.classList.contains("dark-theme");
+  localStorage.setItem("darkMode", isDarkModeEnabled.toString());
 });
